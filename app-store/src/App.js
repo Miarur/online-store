46eb28584e027c2a8115f30484222e2096df3c1b
miarur <deadheartedfo@yahoo.com>
2022-07-14 13:10:32 +0300

    feat: add UI component SortSelect, add styles in SortSelect, add function sorting on year and name

--- app-store/src/App.js
+++ app-store/src/App.js
@@ -3,14 +3,37 @@ import './styles/App.css';
 import CardList from './components/CardList/CardList';
 import axios from 'axios';
 import data from './API/data.json';
+import SortSelect from './components/UI/select/SortSelect';
 
 
 function App() {
 
   const [cards, setCards] = useState(data.sku);
 
-  return (
+  //состояние + двухсторонне связывание для сортировки
+  const [selectedSort, setSelectedSort] = useState('')
+
+  //функция сортировки
+  const sortCards = (sort) => {
+    setSelectedSort(sort);
+    setCards([...cards].sort((a, b) => a[sort].localeCompare(b[sort])))
+  }
+
+  return (    
     <div className="App wrapper">
+      <div>
+        <SortSelect
+          value={selectedSort}
+          onChange={sortCards}
+          defaultValue={'Сортировка...'}
+          options={[
+            {value: 'name', name: 'По названию, от A - Z'},
+            // {value: 'name', name: 'По названию, от Z - A'},
+            {value: 'year', name: 'По дате выхода, по возрастанию'},
+            // {value: 'year', name: 'По дате выхода, по убыванию'}
+          ]}
+        />
+      </div>
       <CardList cards={cards}></CardList>
     </div>
   );
---
+++ app-store/src/components/UI/select/SortSelect.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './SortSelect.module.css'
+
+const SortSelect = ({options, defaultValue, value, onChange}) => {
+  
+  return (
+    <div className='select'>
+      <select
+        value={value}
+        onChange={event => onChange(event.target.value)}
+      >
+        <option disabled value=''>{defaultValue}</option>
+
+        {/* index - заменить потом на уникальный id */}
+        {options.map((option, index) => 
+          <option value={option.value} key={index}>
+          {option.name}
+          </option>  
+        )}
+      </select>
+   </div>
+  );
+}
+
+export default SortSelect;
