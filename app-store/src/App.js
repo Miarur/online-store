71e16150e3cbfaef0ae8657383e7dabb385e85c6
miarur <deadheartedfo@yahoo.com>
2022-07-14 11:41:27 +0300

    feat: add database in json format

--- app-store/src/App.js
+++ app-store/src/App.js
@@ -1,34 +1,13 @@
 import React, {useState} from 'react';
 import './styles/App.css';
 import CardList from './components/CardList/CardList';
+import axios from 'axios';
+import data from './API/data.json';
 
 
 function App() {
 
-  const [cards, setCards] = useState([
-    {
-      name:'Iphone 13',
-      img: 'https://content2.onliner.by/catalog/device/header/b9fefc8c8f96dbc21492792f3a1a502d.jpeg', 
-      quantity: '12', 
-      year: '2021', 
-      producer: 'Apple', 
-      color: 'black', 
-      quantityCamers: 3,
-      popularity: 'yes', 
-    }, 
-
-    {
-      name:'Iphone 11',
-      img: 'https://content2.onliner.by/catalog/device/header/b9fefc8c8f96dbc21492792f3a1a502d.jpeg', 
-      quantity: '10', 
-      year: '2020', 
-      producer: 'Apple', 
-      color: 'black', 
-      quantityCamers: 3,
-      popularity: 'no', 
-    }, 
-
-  ]);
+  const [cards, setCards] = useState(data.sku);
 
   return (
     <div className="App wrapper">
