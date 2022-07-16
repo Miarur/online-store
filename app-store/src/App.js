bfe63ec734794ff3b0a98e702a1a7ecc82a9242d
miarur <deadheartedfo@yahoo.com>
2022-07-16 13:07:28 +0300

    refactor: sorting filter fixed

--- app-store/src/App.js
+++ app-store/src/App.js
@@ -1,36 +1,51 @@
-import React, {useState} from 'react';
-import './styles/App.css';
-import CardList from './components/CardList/CardList';
-import axios from 'axios';
-import data from './API/data.json';
-import SortSelect from './components/UI/select/SortSelect';
-
+import React, { useState } from "react";
+import "./styles/App.css";
+import CardList from "./components/CardList/CardList";
+import axios from "axios";
+import data from "./API/data.json";
+import SortSelect from "./components/UI/select/SortSelect";
 
 function App() {
-
   const [cards, setCards] = useState(data.sku);
 
   //состояние + двухсторонне связывание для сортировки
-  const [selectedSort, setSelectedSort] = useState('')
+  const [selectedSort, setSelectedSort] = useState("");
 
   //функция сортировки
   const sortCards = (sort) => {
-    setSelectedSort(sort);
-    setCards([...cards].sort((a, b) => a[sort].localeCompare(b[sort])))
-  }
+    setSelectedSort(sort)
+    switch (sort) {
+      case "nameUp":
+        return setCards([...cards].sort((a, b) => a.name.localeCompare(b.name)));
+      case "nameDown":
+        return setCards([...cards].sort((a, b) => b.name.localeCompare(a.name)));
+      case "priceUp":
+        return setCards([...cards].sort((a, b) => a.price - b.price));
+      case "priceDown":
+        return setCards([...cards].sort((a, b) => b.price - a.price));
+      case "yearUp":
+        return setCards([...cards].sort((a, b) => a.year - b.year));
+      case "yearDown":
+        return setCards([...cards].sort((a, b) => b.year - a.year));
+      default:
+        return sort;
+    }
+  };
 
-  return (    
+  return (
     <div className="App wrapper">
       <div>
         <SortSelect
           value={selectedSort}
           onChange={sortCards}
-          defaultValue={'Сортировка...'}
+          defaultValue={"Сортировка..."}
           options={[
-            {value: 'name', name: 'По названию, от A - Z'},
-            // {value: 'name', name: 'По названию, от Z - A'},
-            {value: 'year', name: 'По дате выхода, по возрастанию'},
-            // {value: 'year', name: 'По дате выхода, по убыванию'}
+            { value: "nameUp", name: "По названию, от A - Z" },
+            { value: "nameDown", name: "По названию, от Z - A" },
+            { value: "priceUp", name: "По цене, сначала дешевле" },
+            { value: "priceDown", name: "По цене, сначала дороже" },
+            { value: "yearUp", name: "По дате выхода, по возрастанию" },
+            { value: "yearDown", name: "По дате выхода, по убыванию" },
           ]}
         />
       </div>
--- app-store/src/components/UI/select/SortSelect.jsx
+++ app-store/src/components/UI/select/SortSelect.jsx
@@ -1,25 +1,31 @@
-import React from 'react';
-import './SortSelect.module.css'
+import React from "react";
+import "./SortSelect.module.css";
 
 const SortSelect = ({options, defaultValue, value, onChange}) => {
-  
+
   return (
-    <div className='select'>
+    <div className="select">
       <select
         value={value}
         onChange={event => onChange(event.target.value)}
       >
-        <option disabled value=''>{defaultValue}</option>
+        <option disabled value="">
+          {defaultValue}
+        </option>
 
         {/* index - заменить потом на уникальный id */}
         {options.map((option, index) => 
-          <option value={option.value} key={index}>
+          <option
+            value={option.value}  
+            key={index}
+            method={option.method}
+          >
           {option.name}
           </option>  
         )}
       </select>
-   </div>
+    </div>
   );
-}
+};
 
 export default SortSelect;
